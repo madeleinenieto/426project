4aec22d937325ff79623fc1512b7d32de8aa4215
Gillian Montgomery <31824305+gillianm3@users.noreply.github.com>
2019-11-26 17:08:14 -0500

    Update render.js
    This update allows the icon sized Ramses to move left, right, up, down, and to jump. For the sake of the game we will only need right and jump (maybe left if we allow the user to go back in the game)

--- render.js
+++ render.js
@@ -67,41 +67,79 @@ let canvas = document.getElementById('canvas1');
     let imgWidth = 250;
 
     background.src="/backgrounds/Trees.png";
-    ramses.src = "/characters/ramses.png";
+    ramses.src = "/characters/ramsesRight.png";
     artsyGhost.src = "/characters/artsyGhost.png";
 
     background.onload = function(){
         context.drawImage(background,0,0);
-        // context.drawImage(ramses,100,100);
-        context.drawImage(artsyGhost,0,0,artsyGhost.width,artsyGhost.height,-1,125,50,60);
+        context.drawImage(ramses,0,0,ramses.width,ramses.height,-1,142,50,40);
+        // context.drawImage(artsyGhost,0,0,artsyGhost.width,artsyGhost.height,-1,125,50,60);
                                //0,0 is the position in its own rectangle   //-1 is the space on the left
                                                                             //125 is moving down 
                                                                             //50 and 60 are width and hieght respectively
         //artsyGhost.controller();
-    }
-    canvas.focus();
-    
+    }    
+
+
 
     $(document).ready(function(){
 
         $(document).keydown(function(e){
-            var key = e.keyCode;
+            //works for ramses that is in the html but not the canvas loaded Ramses
+            let position = $("#ramses").offset();
+            let min = 0;
+            let max = $('#root').width();
+            let move_amt = 10;
+            let key = e.keyCode;
             if ( key == 37  ){
-                left = true;
                 console.log("left");
-                artsyGhost.style.left = parseInt(artsyGhost.style.left) - 5;
+                let newLeft = ((position.left-move_amt < min)?min:position.left-move_amt);
+                $("#ramses").attr("src",'/characters/ramsesLeft.png');
+                $("#ramses").offset({left:newLeft});
             } else if (key == 38){
-                up = true;
                 console.log("up");
+                $("#ramses").animate({ top: "-=10"},0);
             } else if (key == 39){
-                right = true;
                 console.log("right");
+                var newRight = ((position.left+move_amt > max) ? max : position.left+move_amt);
+                $("#ramses").attr("src",'/characters/ramsesRight.png');
+                $("#ramses").offset({ left: newRight});
+            }
+            else if (key == 40){
+                console.log("down");
+                $("#ramses").animate({ top: "+=10"},0);
+            }
+            else if (key == 32){
+                console.log("spacebar?");
+               $("#ramses").attr("src",'/characters/ramsesRightJump.png');
+               if($("#ramses").animate({top:"-=60"} <= "-=61")){
+                $("#ramses").animate({ top: "-=60"},{duration:4},{complete:$("#ramses").attr("src",'/characters/ramsesRightJump.png')});
+            };
+               //$("#ramses").animate({top:"-=60"},{always:$("#ramses").attr("src",'/characters/ramsesRightJump.png')});
+
+                // $("#ramses").animate({ top: "-=60"},{duration:4},{complete:$("#ramses").attr("src",'/characters/ramsesRightJump.png')});
+
+
+            }
+        });
+        $(document).keyup(function(e){
+            //works for ramses that is in the html but not the canvas loaded Ramses
+
+            let key = e.keyCode;
+            
+          if (key == 32){
+                console.log("spacebar release?");
+               $("#ramses").attr("src",'/characters/ramsesRight.png');
+              $("#ramses").animate({top:"+=60"});
+
+                // $("#ramses").animate({ top: "-=60"},{duration:4},{complete:$("#ramses").attr("src",'/characters/ramsesRightJump.png')});
+
+
             }
         });
         
     });
 
- 
 
     //* this loop function moves the trees as a weird staggard image --> correct direection but wrong implementation
       //  also we want to change it so it moves on a move not just an animation */
